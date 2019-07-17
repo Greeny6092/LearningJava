6bd9bffd704da1077b48f6986bde9c214a0e137d
Greeny6092 <46028156+Greeny6092@users.noreply.github.com>
2019-07-17 06:54:05 -0700

    Add files via upload

--- Day2 Java/FiboSumUsingArrayList.java
+++ Day2 Java/FiboSumUsingArrayList.java
@@ -4,21 +4,24 @@ class FiboSumUsingArrayList
 {
 
 	public static ArrayList<Integer> getFibonacciSeries(int series_len)
-	{	int f1=1,f2=1;
+	{	int f1=1,f2=1,sum;
 		ArrayList<Integer> output= new ArrayList<Integer>();
+		ArrayList<Integer> actual_output= new ArrayList<Integer>();
 		if(series_len==1)
 		{
 			output.add(1);
 			output.add(1);
+			sum=2;
 			return output;
 		}
 		else
 		{
 			output.add(1);
 			output.add(1);
+			sum=2;
 			while(true)
 			{
-				if(f1+f2>series_len)
+				if((f1+f2)>=series_len)
 				{
 					break;
 				}
@@ -26,7 +29,17 @@ class FiboSumUsingArrayList
 				f1=f2;
 				f2=output.get(output.size()-1);
 			}
-			return output;
+			sum=series_len;
+			do
+			{	int t=output.get(output.size()-1);
+				if(t<=sum)
+				{
+					actual_output.add(t);
+					sum-=t;
+				}
+				output.remove(output.size()-1);
+			}while(sum>0);
+			return actual_output;
 		}
 		
 	}	
